4f81e546a495fe0285cd43c2481bf339e0e57be6
ShotaTokuyama <tokush1031@gmail.com>
2023-03-18 20:33:03 +0900

    tailwindcss 適応

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,5 @@ import Payment from './components/Payment.vue'
 </script>
 
 <template>
-    <Payment/>
+  <Payment />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -1,82 +1,56 @@
 <script setup lang="ts">
-import { ref, reactive, computed } from 'vue'
+import { ref, reactive, computed } from "vue";
 
-const url1 = 'https://www.amazon.co.jp/'
+const url1 = "https://www.amazon.co.jp/";
 
 const buy = (itemName: string) => {
-  alert('buy' + itemName)
-}
+  alert("buy" + itemName);
+};
 
-const item1 = reactive ({
-  name: 'Desk',
+const item1 = reactive({
+  name: "Desk",
   price: 40000,
-})
+});
 
-const item2 = reactive ({
-  name: 'Bike',
+const item2 = reactive({
+  name: "Bike",
   price: 20000,
-}) 
-
+});
 
 const clear = () => {
-  item1.name = ""
-  item1.price = 0
-}
+  item1.name = "";
+  item1.price = 0;
+};
 
-const budget = 50000
+const budget = 50000;
 const priceLabel = computed(() => {
-  if(item1.price > budget) {
-    return 'too expensive...'
+  if (item1.price > budget) {
+    return "too expensive...";
   } else {
-    return item1.price + 'yen'
+    return item1.price + "yen";
   }
-})
+});
 </script>
 
 <template>
-  <div class="container">
+  <div class="flex flex-col items-center">
     <h1>最近の支出</h1>
     <div class=""></div>
     <label for="itemName">アイテム</label>
-    <input v-model="item1.name">
+    <input v-model="item1.name" />
     <label for="itemPrice">価格</label>
-    <input v-model="item1.price"/>
+    <input v-model="item1.price" class="ml-2" />
     <button v-on:click="clear">Clear</button>
-    <div class="payment">
-      <label>{{ item1.name }}</label>
-      <label>{{ priceLabel }}</label>
+    <div class="flex justify-between items-center w-96 h-20 bg-red-200">
+      <label class="text-xl font-bold">{{ item1.name }}</label>
+      <label class="text-xl font-bold">{{ priceLabel }}</label>
       <a v-bind:href="url1">bought at...</a>
       <button v-on:click="buy(item1.name)">BUY</button>
     </div>
-    <div class="payment">
-      <label>{{ item2.name }}</label>
-      <label>{{ item2.price }} yen</label>
+    <div class="flex justify-between items-center w-96 h-20 bg-red-200">
+      <label class="text-xl font-bold">{{ item2.name }}</label>
+      <label class="text-xl font-bold">{{ item2.price }} yen</label>
       <button v-on:click="buy(item2.name)">BUY</button>
     </div>
   </div>
 </template>
-
-<style scoped>
-.container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-.payment {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 400px;
-  height: 80px;
-  background-color: pink;
-}
-
-input {
-  margin-bottom: 8px;
-}
-
-label {
-  font-size: 20px;
-  font-weight: bold;
-}
-</style>
