cf4612242de628416db62f15d848d20f215987c0
ShotaTokuyama <tokush1031@gmail.com>
2023-03-18 19:51:46 +0900

    tailwindowcss add

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -1,13 +1,57 @@
-<!-- <script setup lang="ts">
-console.log(1)
-</script> -->
+<script setup lang="ts">
+import { ref, reactive, computed } from 'vue'
+
+const url1 = 'https://www.amazon.co.jp/'
+
+const buy = (itemName: string) => {
+  alert('buy' + itemName)
+}
+
+const item1 = reactive ({
+  name: 'Desk',
+  price: 40000,
+})
+
+const item2 = reactive ({
+  name: 'Bike',
+  price: 20000,
+}) 
+
+
+const clear = () => {
+  item1.name = ""
+  item1.price = 0
+}
+
+const budget = 50000
+const priceLabel = computed(() => {
+  if(item1.price > budget) {
+    return 'too expensive...'
+  } else {
+    return item1.price + 'yen'
+  }
+})
+</script>
 
 <template>
   <div class="container">
     <h1>最近の支出</h1>
+    <div class=""></div>
+    <label for="itemName">アイテム</label>
+    <input v-model="item1.name">
+    <label for="itemPrice">価格</label>
+    <input v-model="item1.price"/>
+    <button v-on:click="clear">Clear</button>
     <div class="payment">
-      <label>カレーライス</label>
-      <label>400円</label>
+      <label>{{ item1.name }}</label>
+      <label>{{ priceLabel }}</label>
+      <a v-bind:href="url1">bought at...</a>
+      <button v-on:click="buy(item1.name)">BUY</button>
+    </div>
+    <div class="payment">
+      <label>{{ item2.name }}</label>
+      <label>{{ item2.price }} yen</label>
+      <button v-on:click="buy(item2.name)">BUY</button>
     </div>
   </div>
 </template>
@@ -25,6 +69,10 @@ console.log(1)
   width: 400px;
   height: 80px;
   background-color: pink;
+}
+
+input {
+  margin-bottom: 8px;
 }
 
 label {
