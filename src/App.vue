ffc71f46642ec3a65fd03499d8870798794d3c90
ShotaTokuyama <tokush1031@gmail.com>
2023-03-19 18:58:53 +0900

    tweets

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import Payment from './components/Payment.vue'
+import Tweet from './components/Tweet.vue'
 </script>
 
 <template>
+  <Tweet />
   <Payment />
 </template>
--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -33,24 +33,21 @@ const priceLabel = computed(() => {
 </script>
 
 <template>
-  <div class="flex flex-col items-center">
-    <h1>最近の支出</h1>
-    <div class=""></div>
-    <label for="itemName">アイテム</label>
-    <input v-model="item1.name" />
-    <label for="itemPrice">価格</label>
-    <input v-model="item1.price" class="ml-2" />
-    <button v-on:click="clear">Clear</button>
+  <div class="flex flex-col items-center container">
+    <h1 class="mt-10 mb-6 text-x3 font-bold">最近の支出</h1>
+    <input class="border-gray-600 border mb-4" v-model="item1.name" />
+    <input class="border-gray-600 border mb-4" v-model="item1.price" />
+    <button class="mb-6 font-bold border-gray-600 border" v-on:click="clear">Clear</button>
     <div class="flex justify-between items-center w-96 h-20 bg-red-200">
       <label class="text-xl font-bold">{{ item1.name }}</label>
       <label class="text-xl font-bold">{{ priceLabel }}</label>
       <a v-bind:href="url1">bought at...</a>
-      <button v-on:click="buy(item1.name)">BUY</button>
+      <button class="border-gray-600 border p-1" v-on:click="buy(item1.name)">BUY</button>
     </div>
     <div class="flex justify-between items-center w-96 h-20 bg-red-200">
       <label class="text-xl font-bold">{{ item2.name }}</label>
       <label class="text-xl font-bold">{{ item2.price }} yen</label>
-      <button v-on:click="buy(item2.name)">BUY</button>
+      <button class="border-gray-600 border p-1" v-on:click="buy(item2.name)">BUY</button>
     </div>
   </div>
 </template>
